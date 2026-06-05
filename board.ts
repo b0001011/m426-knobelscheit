ffe0fbe525b8226ab241e14ce919764ebc28c493
b <brt.ozgr@hotmail.com>
2026-06-05 20:23:55 +0200

    board + game

--- board.ts
+++ board.ts
@@ -5,4 +5,29 @@ export class Board {
     getRemaining(): number[] {
         return this.remaining;
     }
+
+    flip(numbers: number[], target: number): boolean {
+        for (const num of numbers) {
+            if (!this.remaining.includes(num)) {
+                return false;
+            }
+        }
+
+        let sum = 0;
+        for (const num of numbers) {
+            sum += num;
+        }
+
+        if (sum !== target) {
+            return false
+        }
+
+        this.remaining = this.remaining.filter((num) => !numbers.includes(num));
+        return true;
+    }
+
+    isGameOver(): boolean {
+        return this.remaining.length == 0;
+    }
+
 }
